17eb01498a4f9b000cce745aec93873926c95356
chandi <git@chandi.it>
2023-05-08 18:05:08 +0200

    rename roms-availability file

--- azadidroid-data/src/scripts/update_rom_availability.ts
+++ azadidroid-data/src/scripts/update_rom_availability.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const file = path.join(__dirname, '../roms-list.json')
+const file = path.join(__dirname, '../roms-availability.json')
 
 const modelIndex = getModelIndex()
 
