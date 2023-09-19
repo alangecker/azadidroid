10539dd02faf13b98f1b7469b3598523a95f4061
chandi <git@chandi.it>
2023-09-19 11:58:37 +0200

    loading bar on page change

--- azadidroid-web/src/layouts/default/Default.vue
+++ azadidroid-web/src/layouts/default/Default.vue
@@ -9,10 +9,33 @@
           <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
           <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
         </v-list> -->
+    <div v-if="isLoading" class="loading-bar">
+      <v-progress-linear height="3" indeterminate></v-progress-linear>
+    </div>
   </v-app>
 </template>
 
 <script lang="ts" setup>
   import DefaultBar from './AppBar.vue'
   import DefaultView from './View.vue'
+
+  import router from '../../router'
+  import { ref } from 'vue'
+
+  const isLoading = ref(false)
+  router.beforeEach(() => {
+    isLoading.value = true
+  })
+  router.afterEach(() => {
+    isLoading.value = false
+  })
 </script>
+<style scoped>
+.loading-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 2000;
+}
+</style>
